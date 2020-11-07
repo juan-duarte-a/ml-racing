b676511622491cc3da05136a8c8b994317399813
Juan Duarte <juan.duarte.a@gmail.com>
2020-11-06 19:29:50 -0500

    Improvement to rotation code
    Optimization of rotation code.
    Angle automatic calculated according to turn angle parameter.
    Use of Vector2 methods instead of own ones.

--- src/Car.gd
+++ src/Car.gd
@@ -6,33 +6,19 @@ export var turn_angle: float = 30
 
 var velocity: Vector2
 var direction: Vector2
-var _vector: int
 
 enum ACTIONS {RUN, STOP, TURN_RIGHT, TURN_LEFT}
-var DIRECTIONS: Array = [
-	Vector2(0, 1),
-	Vector2(-0.5, 0.86602540378),
-	Vector2(-0.86602540378, 0.5),
-	Vector2(-1, 0),
-	Vector2(-0.86602540378, -0.5),
-	Vector2(-0.5, -0.86602540378),
-	Vector2(0, -1),
-	Vector2(0.5, -0.86602540378),
-	Vector2(0.86602540378, -0.5),
-	Vector2(1, 0),
-	Vector2(0.86602540378, 0.5),
-	Vector2(0.5, 0.86602540378)
-]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	direction = Vector2(-1, 0)
 	velocity = Vector2(0, 0)
-	_vector = 3
-	set_rotation_degrees(-90)
+
 
 
 func set_action(action: int):
+	
 	if action == ACTIONS.RUN:
 		if velocity == Vector2.ZERO:
 			velocity = direction * speed
@@ -41,21 +27,15 @@ func set_action(action: int):
 	elif action == ACTIONS.STOP:
 		velocity = Vector2.ZERO
 	elif action == ACTIONS.TURN_LEFT:
-		if _vector == 0:
-			_vector = DIRECTIONS.size() - 1
-		else:
-			_vector -= 1
-		direction = DIRECTIONS[_vector]
-		set_rotation_degrees(rotation_degrees - turn_angle)
+		direction = direction.rotated(deg2rad(-turn_angle))
+#		set_rotation_degrees(rotation_degrees - turn_angle)
+		print(direction)
 		if velocity != Vector2.ZERO:
 			velocity = direction * speed
 	elif action == ACTIONS.TURN_RIGHT:
-		if _vector == DIRECTIONS.size() - 1:
-			_vector = 0
-		else:
-			_vector += 1
-		direction = DIRECTIONS[_vector]
-		set_rotation_degrees(rotation_degrees + turn_angle)
+		direction = direction.rotated(deg2rad(turn_angle))
+#		set_rotation_degrees(rotation_degrees + turn_angle)
+		print(direction)
 		if velocity != Vector2.ZERO:
 			velocity = direction * speed
 
@@ -71,8 +51,10 @@ func _process(delta):
 	elif Input.is_action_just_pressed("turn_left"):
 		print("LEFT: ", OS.get_ticks_msec())
 		set_action(ACTIONS.TURN_LEFT)
+		set_rotation_degrees(rad2deg(direction.angle()))
 	elif Input.is_action_just_pressed("turn_right"):
 		print("RIGHT: ", OS.get_ticks_msec())
 		set_action(ACTIONS.TURN_RIGHT)
+		set_rotation_degrees(rad2deg(direction.angle()))
 	
 	velocity = move_and_slide(velocity)
