fea5e49f2cedb12b261530fe9cd1d4a35d687cff
Juan Duarte <juan.duarte.a@gmail.com>
2020-11-09 02:34:34 -0500

    Improved car collision
    Changed method use for moving the car (move_and_collide).
    Speed factor is now applied only in physics process.

--- src/Car.gd
+++ src/Car.gd
@@ -14,6 +14,7 @@ var turning: bool
 var turning_left: bool
 var speed_factor: float
 var prev_speed_factor: float
+var collision_object: KinematicCollision2D
 
 
 enum ACTIONS {RUN, STOP, TURN_RIGHT, TURN_LEFT}
@@ -32,9 +33,9 @@ func _ready():
 func set_action(action: int):
 	if action == ACTIONS.RUN:
 		if velocity == Vector2.ZERO:
-			velocity = direction * _speed * speed_factor
+			velocity = direction * _speed
 		else:
-			velocity = velocity.normalized() * _speed * speed_factor
+			velocity = velocity.normalized() * _speed
 	elif action == ACTIONS.STOP:
 		velocity = Vector2.ZERO
 	elif action == ACTIONS.TURN_LEFT:
@@ -43,14 +44,14 @@ func set_action(action: int):
 			turning_left = true
 #			print("Turning start time: ", OS.get_ticks_msec())
 		if velocity != Vector2.ZERO:
-			velocity = direction * _speed * speed_factor
+			velocity = direction * _speed
 	elif action == ACTIONS.TURN_RIGHT:
 		if turning == false:
 			turning = true
 			turning_left = false
 #			print("Turning start time: ", OS.get_ticks_msec())
 		if velocity != Vector2.ZERO:
-			velocity = direction * _speed * speed_factor
+			velocity = direction * _speed
 
 
 func update_turn_angle(delta: float, left: bool, turn_angle: float = _turn_angle):
@@ -74,22 +75,22 @@ func update_turn_angle(delta: float, left: bool, turn_angle: float = _turn_angle
 		if abs(direction.angle_to(Vector2.UP)) < deg2rad(0.001):
 			direction = Vector2.UP
 			if velocity != Vector2.ZERO:
-				velocity = Vector2.UP * _speed * speed_factor
+				velocity = Vector2.UP * _speed
 			set_rotation_degrees(-90)
 		elif abs(direction.angle_to(Vector2.RIGHT)) < deg2rad(0.001):
 			direction = Vector2.RIGHT
 			if velocity != Vector2.ZERO:
-				velocity = Vector2.RIGHT * _speed * speed_factor
+				velocity = Vector2.RIGHT * _speed
 			set_rotation_degrees(0)
 		elif abs(direction.angle_to(Vector2.DOWN)) < deg2rad(0.001):
 			direction = Vector2.DOWN
 			if velocity != Vector2.ZERO:
-				velocity = Vector2.DOWN * _speed * speed_factor
+				velocity = Vector2.DOWN * _speed
 			set_rotation_degrees(90)
 		elif abs(direction.angle_to(Vector2.LEFT)) < deg2rad(0.001):
 			direction = Vector2.LEFT
 			if velocity != Vector2.ZERO:
-				velocity = Vector2.LEFT * _speed * speed_factor
+				velocity = Vector2.LEFT * _speed
 			set_rotation_degrees(180)
 #		print(direction)
 #		print("Turning finish time: ", OS.get_ticks_msec())
@@ -100,7 +101,11 @@ func set_speed(speed: int):
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+#func _process(delta):
+#	pass
+
+
+func _physics_process(delta):
 #	print(road.get_cellv(road.world_to_map(Vector2(position.x, position.y - 60))) == -1)
 	if road.get_cellv(road.world_to_map(Vector2(position.x, position.y - 60))) == -1:
 		speed_factor = 0.6
@@ -125,9 +130,9 @@ func _process(delta):
 	
 	if prev_speed_factor != speed_factor:
 		if speed_factor == 1:
-			velocity = move_and_slide(velocity * (1/prev_speed_factor))
+			collision_object = move_and_collide(velocity * delta * (1/prev_speed_factor))
 		else:
-			velocity = move_and_slide(velocity * speed_factor)
+			collision_object = move_and_collide(velocity * delta * speed_factor)
 		prev_speed_factor = speed_factor
 	else:
-		velocity = move_and_slide(velocity)
+		collision_object = move_and_collide(velocity * delta * speed_factor)
